49e5c2120a263070d0da8e6c998ba7f96e61c98f
TimotiusEk <eka.valentino@gmail.com>
2023-03-07 12:17:44 +0700

    Added stream routes to master routes

--- src/routes/v1/routes.ts
+++ src/routes/v1/routes.ts
@@ -2,11 +2,13 @@ import express from "express";
 import routesAuth from "./auth.routes";
 import routesUtil from "./util.routes";
 import routesEvent from "./event.routes";
+import routesStream from "./stream.routes";
 
 const router = express.Router();
 
 routesAuth(router);
 routesUtil(router);
 routesEvent(router);
+routesStream(router);
 
 export default router;
--- src/routes/v1/stream.routes.ts
+++ src/routes/v1/stream.routes.ts
@@ -1,8 +1,8 @@
-import {Express} from "express";
+import {Express, Router} from "express";
 import PipelineController from "../../controllers/pipeline.controller";
 
-export default function (app : Express) {
-    app.route('/stream/:node_num/:id/pipeline/:code')
+export default function (router : Router) {
+    router.route('/stream/:node_num/:id/pipeline/:code')
         .get(PipelineController.getPipeline)
         .put(PipelineController.updatePipeline);
 }
