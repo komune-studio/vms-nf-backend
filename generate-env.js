20e1cf66baa349210e27fdb6f54211403ac0a0a9
TimotiusEk <eka.valentino@gmail.com>
2023-03-21 10:26:10 +0700

    Added default SECRET_KEY when docker run doesn't contain the key

--- generate-env.js
+++ generate-env.js
@@ -5,6 +5,10 @@ const env = {
     DATABASE_URL_NFVISIONAIRE_WITH_SCHEMA: `postgresql://postgres:nfvisionaire123@${process.env.SERVER_PORT}:${process.env.DB_PORT}/nfvisionaire`
 }
 
+if(process.env.SECRET_KEY) {
+    env.SECRET_KEY = `"aHsJ09wtn0286735384h.jHF42hynwMntgY"`
+}
+
 let fileContent = '';
 
 Object.keys(env).forEach(key => {
