673007cf526287b94535c30d368f3ff5dfeb82cb
Raffael Fradio <fradior.xd@gmail.com>
2023-03-21 17:48:00 +0700

    Add superadmin (2)

--- src/routes/v1/site.routes.ts
+++ src/routes/v1/site.routes.ts
@@ -1,17 +1,17 @@
 import {Router} from "express";
 import SiteController from "../../controllers/site.controller";
-import {authAdmin, authAll} from "../../middlewares/auth.middleware";
+import {authAdmin, authAll, authSuperAdmin} from "../../middlewares/auth.middleware";
 export default function routesSite(router : Router) {
     router.route('/sites')
         .get(authAll, SiteController.getAll)
 
     router.route('/site')
-        .post(authAdmin, SiteController.create)
+        .post(authSuperAdmin, SiteController.create)
 
     router.route('/site/:id')
         .get(authAdmin, SiteController.getById)
-        .put(authAdmin, SiteController.update)
-        .delete(authAdmin, SiteController.delete)
+        .put(authSuperAdmin, SiteController.update)
+        .delete(authSuperAdmin, SiteController.delete)
 
     router.route('/site/:id/assign-stream')
         .post(authAdmin, SiteController.assignStream)
