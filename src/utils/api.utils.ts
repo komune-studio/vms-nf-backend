16131de287963f399b46d492fe4d90f348823993
TimotiusEk <eka.valentino@gmail.com>
2023-05-03 14:37:41 +0700

    Remove unnecessary console.log

--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -35,10 +35,7 @@ export async function requestWithFile(endpoint : string, method : string, body :
         body: body,
     }
 
-    console.log('test')
     let response = await nodefetch(endpoint, request);
-
-    console.log(response)
 
     response = await response.json();
 
