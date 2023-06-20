bbeb1e0e77ed29a5de21cc8930f3b59aa0298cba
TimotiusEk <eka.valentino@gmail.com>
2023-06-20 16:27:14 +0700

    Bug fixing where generate env is not working properly

--- generate-env.js
+++ generate-env.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
 
 const env = {
-    DATABASE_URL_NFV4_WITH_SCHEMA: `postgresql://postgres:nfvisionaire123@${process.env.SERVER_PORT}:${process.env.DB_PORT}/nfv4`,
-    DATABASE_URL_NFVISIONAIRE_WITH_SCHEMA: `postgresql://postgres:nfvisionaire123@${process.env.SERVER_PORT}:${process.env.DB_PORT}/nfvisionaire`
+    DATABASE_URL_NFV4_WITH_SCHEMA: `postgresql://postgres:nfvisionaire123@${process.env.NF_IP}:${process.env.DB_PORT}/nfv4`,
+    DATABASE_URL_NFVISIONAIRE_WITH_SCHEMA: `postgresql://postgres:nfvisionaire123@${process.env.NF_IP}:${process.env.DB_PORT}/nfvisionaire`,
+    NF_IP: process.env.NF_IP,
+    VANILLA_PORT: process.env.VANILLA_PORT,
+    VISIONAIRE_PORT: process.env.VISIONAIRE_PORT,
+    NF_VISIONAIRE_API_URL: `http://${process.env.NF_IP}:${process.env.VISIONAIRE_PORT}`,
+    NF_VANILLA_API_URL: `http://${process.env.NF_IP}:${process.env.VANILLA_PORT}/api`,
+    NF_FREMISN_API_URL: `http://${process.env.NF_IP}:${process.env.FREMISN_PORT}/v1`,
 }
 
-if(process.env.SECRET_KEY) {
+if(!process.env.SECRET_KEY) {
     env.SECRET_KEY = `"aHsJ09wtn0286735384h.jHF42hynwMntgY"`
 }
 
