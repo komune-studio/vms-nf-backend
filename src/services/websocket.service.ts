e4aaf8fbdadf962c1e64d7bed25f8486d53280a8
TimotiusEk <eka.valentino@gmail.com>
2023-09-05 23:56:31 +0700

    Added rtsp websocket

--- src/services/websocket.service.ts
+++ src/services/websocket.service.ts
@@ -1,18 +1,13 @@
-import moment from "moment";
-import {client as WebsocketClient, connection, server as WebsocketServer} from "websocket";
+import {connection, server as WebsocketServer} from "websocket";
 import * as http from "http";
-import EnrolledFaceDAO from "../daos/enrolled_face.dao";
-import RecognizedEventDAO from "../daos/recognized_event.dao";
-import UnrecognizedEventDAO from "../daos/unrecognized_event.dao";
-import FremisnDAO from "../daos/fremisn.dao";
 
-const requestUrl = `ws://${process.env['NF_IP']}:${process.env['VISIONAIRE_PORT']}/event_channel`;
+const rtsp = require('rtsp-ffmpeg');
 
 export default class WebsocketService {
     private static instance: WebsocketService;
-    private client : WebsocketClient;
     private server : WebsocketServer;
     private connections : connection[];
+    private rtsp : any[];
 
     static getInstance(): WebsocketService | null {
         if (!WebsocketService.instance) {
@@ -31,91 +26,56 @@ export default class WebsocketService {
     private constructor(server : http.Server) {
 
         this.connections = [];
+        this.rtsp = []; //
 
-        this.client = new WebsocketClient();
         this.server = new WebsocketServer({
             httpServer: server,
             autoAcceptConnections: false,
         });
 
-        this.client.on('connectFailed', (error) => {
-            console.log('Connect Error: ' + error.toString());
-            console.log("Retrying in 5 seconds...");
-            setTimeout(() => {
-                this.client.connect(requestUrl);
-            }, 5000);
-        });
-
-        this.client.on('connect', (connection) => {
-            console.log('Websocket Client Connected');
-
-            connection.on('error', (error) => {
-                console.log("Connection Error: " + error.toString());
-                console.log("Retrying in 5 seconds...");
-                setTimeout(() => {
-                    this.client.connect(requestUrl);
-                }, 5000);
-            });
-            connection.on('close', () => {
-                console.log("Client disconnected. Retrying in 5 seconds...");
-                setTimeout(() => {
-                    this.client.connect(requestUrl);
-                }, 5000);
-            });
-            connection.on('message', async (message) => {
-                if (message.type !== 'utf8') return;
-                const data = JSON.parse(message.utf8Data);
-
-                let payload : any;
-
-                if(data.analytic_id === 'NFV4-FR' || data.analytic_id === 'NFV4H-FR') {
-                    payload = {
-                        timestamp: data.timestamp,
-                        stream_name: data.stream_name,
-                        image: Buffer.from(data.image_jpeg, 'base64')
-                    }
-
-                    const response = await FremisnDAO.faceEnrollment(data.pipeline_data.status === 'KNOWN' ? 'recognized' : 'unrecognized', data.image_jpeg)
-                    payload.face_id = BigInt(response.face_id)
-
-                    if(data.pipeline_data.status === 'KNOWN') {
-                        const face = await EnrolledFaceDAO.getByFaceId(data.pipeline_data.face_id);
-
-                        if(!face) return;
-
-                        payload.enrollment_id = face.id
-
-                        await RecognizedEventDAO.create(payload)
-                    } else {
-                        await UnrecognizedEventDAO.create(payload)
-                    }
-                }
-
-                // console.log(payload)
-                console.log(this.connections.length)
-                this.connections.forEach(c => c.sendUTF(JSON.stringify(payload)));
-            });
-        });
-
         this.server.on('request', (request) => {
-            // Note: add origin verification if necessary
-            // if (!originIsAllowed(request.origin)) {
-            //     // Make sure we only accept requests from an allowed origin
-            //     request.reject();
-            //     console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-            //     return;
-            // }
+            // @ts-ignore
+            let {stream, start_time, end_time} = request.resourceURL.query;
 
             const connection = request.accept();
+
+            if(stream) {
+                stream = `${stream}?starttime=${start_time}&endtime=${end_time}`
+
+                console.log(stream)
+
+                // @ts-ignore
+                connection.stream = stream
+
+                if(this.rtsp.filter(data => data.stream === stream).length === 0) {
+                    const ffmpeg = new rtsp.FFMpeg({input: stream});
+                    const connections = this.connections;
+                    const pipeStream = function(data : any) {
+                        // @ts-ignore
+                        connections.filter(connection => connection.stream === stream).forEach(c => c.sendUTF(JSON.stringify({
+                            image: data.toString('base64')
+                        })));
+                    };
+
+                    ffmpeg.on('data', pipeStream);
+
+                    this.rtsp.push({stream, ffmpeg, pipeStream});
+                }
+            }
+
             connection.on('close', () => {
                 this.connections = this.connections.filter(c => c !== connection);
+
+                // @ts-ignore
+                if(this.connections.filter(c => stream === c.stream).length === 0) {
+                    const {ffmpeg, pipeStream} =  this.rtsp.filter(data => data.stream === stream)[0];
+                    ffmpeg.removeListener('data', pipeStream);
+
+                    this.rtsp = this.rtsp.filter(r => r.stream !== stream);
+                }
             });
             console.log((new Date()) + ' Connection accepted from ' + request.origin + '.');
             this.connections.push(connection);
         });
-
-        this.client.connect(requestUrl);
     }
-
-
 }
