bd577d891af6d022e84c01f0f4f1dd3f6cb540f0
Zuhdi <rakazhp@gmail.com>
2023-09-13 11:19:08 +0700

    Completed API to get the list of recordings from a DVR

--- src/controllers/playback.controller.ts
+++ src/controllers/playback.controller.ts
@@ -7,8 +7,7 @@ import {requestWithXML} from "../utils/api.utils";
 import {NotFoundError} from "../utils/error.utils";
 // @ts-ignore
 import { toXML } from 'jstoxml';
-import {xml2js} from "xml-js";
-
+import {xml2json} from "xml-js";
 
 export default class PlaybackController {
     static async getRTSPUrls(req: Request, res: Response, next: NextFunction) {
@@ -21,10 +20,11 @@ export default class PlaybackController {
             let xmlBody = toXML(body, config)
 
             let responseFromDVR = await requestWithXML("http://192.168.103.244/ISAPI/ContentMgmt/search", "POST", xmlBody)
-            // @ts-ignore
-            let convertResponseToJSON = xml2js(responseFromDVR)
 
-            res.send(convertResponseToJSON)
+            // @ts-ignore
+            let result1 = xml2json(responseFromDVR, {compact: true, spaces: 4});
+
+            res.send(JSON.parse(result1))
         } catch (err) {
             return next(err);
         }
