4590e194fd21f93edab2665daa0a8dbd6f5dba90
komune <komunestudio@gmail.com>
2026-02-10 17:31:33 +0700

    Added generate env and fetch LPR from API

--- generate-env.js
+++ generate-env.js
@@ -9,6 +9,7 @@ const env = {
     NF_VISIONAIRE_API_URL: `http://${process.env.NF_IP}:${process.env.VISIONAIRE_PORT}`,
     NF_VANILLA_API_URL: `http://${process.env.NF_IP}:${process.env.VANILLA_PORT}/api`,
     NF_FREMISN_API_URL: `http://${process.env.NF_IP}:${process.env.FREMISN_PORT}/v1`,
+    NF_LPR_API_URL: `http://${process.env.NF_IP}:${process.env.LPR_PORT}`,
 }
 
 if(!process.env.SECRET_KEY) {
--- src/daos/lpr.dao.ts
+++ src/daos/lpr.dao.ts
@@ -7,7 +7,7 @@ const admins = prisma.admin;
 export default class LprDAO {
     static async licensePlateRecognition(image : string) {
         try {
-            let result = await request(`${process.env.NF_LPR_API_URL}/license-plate-recognition`, 'POST', {images: [image]});
+            let result = await request(`${process.env.NF_LPR_API_URL}/NFFS-LPRF/v1/license-plate-recognition`, 'POST', {images: [image]});
 
             return result
         } catch (e) {
